2d7654e51e183d359ae2c5a99affdfee92728039
lin bu bin <289873007@qq.com>
2017-04-25 07:02:05 -0700

    shide

---
+++ controllers/session_controller.ex
@@ -0,0 +1,39 @@
+defmodule SimpleAuth.SessionController do
+	import Comeonin.Bcrypt, only: [checkpw: 2,
+	dummy_checkpw: 0]
+	alias SimpleAuth.User
+	use SimpleAuth.Web, :controller
+	plug :scrub_params, "session" when action in
+	~w(create)a
+
+	def new(conn, _) do
+		render conn, "new.html"
+	end
+
+	def create(conn, %{"session" => %{"email" => email,
+	"password" =>
+	password}}) do
+		case SimpleAuth.Auth.login_by_email_and_pass(conn,
+		email,
+		password) do
+			{:ok, conn} ->
+				conn
+				|> put_flash(:info, "You’re now signed in!")
+				|> redirect(to: page_path(conn, :index))
+			{:error, _reason, conn} ->
+				conn
+				|> put_flash(:error, "Invalid email/password
+				combination")
+				|> render("new.html")
+		end
+	end
+
+	def delete(conn, _) do
+		conn
+		|> SimpleAuth.Auth.logout
+		|> put_flash(:info, "See you later!")
+		|> redirect(to: page_path(conn, :index))
+	end
+
+	
+end
---
+++ router.ex
@@ -0,0 +1,61 @@
+defmodule SimpleAuth.Router do
+  use SimpleAuth.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :with_session do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+    plug SimpleAuth.CurrentUser
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  pipeline :login_required do
+    plug Guardian.Plug.EnsureAuthenticated, handler: SimpleAuth.GuardianErrorHandler
+  end
+  pipeline :admin_required do
+    plug SimpleAuth.CheckAdmin
+  end
+
+
+  scope "/", SimpleAuth do
+    pipe_through [:browser, :with_session] # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:new, :create]
+    resources "/sessions", SessionController, only: [:new, :create,:delete]
+
+    # registered user zone
+    scope "/" do
+      pipe_through [:login_required]
+
+      resources "/users", UserController, only: [:show] do
+        resources "/posts", PostController
+      end
+    
+
+    # admin zone
+      scope "/admin", Admin, as: :admin do
+        pipe_through [:admin_required]
+
+        resources "/users", UserController, only: [:index, :show] do
+          resources "/posts", PostController, only: [:index, :show]
+        end
+      end
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SimpleAuth do
+  #   pipe_through :api
+  # end
+end
